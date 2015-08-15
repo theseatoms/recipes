aa0b0dc839af7c588838c1db79f1e91c92368faf
TS <theseatoms@users.noreply.github.com>
2015-08-14 21:36:42 -0500

    Create scrape_forks.py

---
+++ scrape_forks.py
@@ -0,0 +1,10 @@
+# Script that will traverse all forks of LarryMad/recipies,
+#                  collect unique recipies,
+#                  and save them in a directory in this repo
+
+#TODO: define unique-ness by file contents, vs filename (first implementation)
+
+import json
+import urllib2
+
+access_token= <FILL IN>
