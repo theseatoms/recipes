f62d7e0156f3a6c22a597b1dcc00ba2adf4edde4
Tony Salvatore <tonypsalvatore@gmail.com>
2015-08-17 18:18:11 -0500

    implemented contents. about to do oauth.

--- scrape_forks.py
+++ scrape_forks.py
@@ -5,12 +5,11 @@ from __future__ import print_function
 #                  and save them in a directory in this repo
 
 #TODO:  define unique-ness by file contents, vs filename (first implementation)
-
 #TODO: pagination of forks data... am i getting all forks?
 
+import os
 import json
 import urllib2
-
 
 def api(REST_call):
     api_root = "https://api.github.com"
@@ -27,63 +26,75 @@ def api_url(query_url):
 
 # -------------------------------------------------------------
 
-#access_token = <FILL IN>
 #TODO implement authentication
+#access_token = <FILL IN>
 
 data = api("/repos/LarryMad/recipes")
 larry_forks_url = data["forks_url"]
 larry_forks_data = api_url(larry_forks_url)
 
-filename = "recipe_paths"
-
+output_filename = "recipe_paths"
 
 for fork_data in larry_forks_data:
     GET_sha = "/repos/" + fork_data["full_name"] + "/git/refs/"
-    print("\n\n", GET_sha)
     data = api(GET_sha)
-    print(data)
+    
     sha = -1
     for dat in data:
         if dat["ref"] == "refs/heads/master":
             sha = dat["object"]["sha"]
             break
     if sha == -1:
-        print("no 'refs/heads/master' found")
+        print("no 'refs/heads/master' found for " + str(fork_data) + "\n" + str(dat))
         continue
-
 
     GET_commit = "/repos/" + fork_data["full_name"] + "/git/commits/" + sha
     data = api(GET_commit)
     
-    tree_sha = data["tree"]["sha"] #?????  but why?,,,, to ensure latest, "master" branch?
+    tree_sha = data["tree"]["sha"] # why? to ensure latest, "master" branch
 
-    GET_tree_contents = "/repos/" + fork_data["full_name"] + "/git/trees/" + tree_sha
-    data = api(GET_tree_contents)
+    GET_tree = "/repos/" + fork_data["full_name"] + "/git/trees/" + tree_sha
+    data = api(GET_tree)
+
 
     paths = [] 
 
     for file_data in data["tree"]:
-        #print(file_data["path"], file=f)
+        print(file_data) 
+        print(file_data["url"])
         
-        f = open(filename,'a')
-        f.write(file_data["path"]+'\n') 
+        GET_contents = "/repos/" + fork_data["full_name"] + "/contents/" + file_data["path"]
+        contents = api(GET_file_contents)
+
+        owner = fork_data["full_name"].split("/")[0] # TODO check this
+        print("owner: "+ owner )
+        
+        new_file_dir = os.getcwd() + "/" + fork_data["full_name"] + "/"
+        new_file_path = os.getcwd() + "/" + fork_data["full_name"] + "/" + file_data["path"] 
+        print(new_file_path)
+        
+        if not os.path.exists(new_file_dir):
+            os.makedirs(new_file_dir) 
+
+        f = open(new_file_path, 'ab+')
+        f.write(contents) 
         f.close()
          
         paths.append( file_data["path"] )
         
-        
-        # pick it up from here...............................TODO TODO TODO 
      
     # get recipes content
     # write recipes 
 
 
+# known issues: appending on each run of the script
 
 # useful links:
 # http://stackoverflow.com/questions/25022016/get-all-file-names-from-a-github-repo-through-the-github-api
 
 
-
+#alternate file_write
+#print(file_data["path"], file=f)
 
 
 
