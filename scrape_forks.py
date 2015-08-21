5417c8b8a90100ceaaca1045bd942f5647f230e9
Tony Salvatore <tonypsalvatore@gmail.com>
2015-08-20 19:09:29 -0500

    traverses pages, but might not start at page 1

--- scrape_forks.py
+++ scrape_forks.py
@@ -5,12 +5,12 @@ from __future__ import print_function
 #                  and save them in a directory in this repo
 
 #TODO:  define unique-ness by file contents, vs filename (first implementation)
-#TODO: pagination of forks data... am i getting all forks?
 
 import os
 import json
 import urllib2
-#from requests_oauthlib import OAuth2Session
+import re
+#from requests_oauthlib import OAuth2Session # TODO someday
 import config
 import requests
 from requests.auth import HTTPBasicAuth
@@ -30,26 +30,38 @@ def api(REST_call, include_header=False):
     response = requests.get(query_url, auth=('theseatoms', config.PASSWORD))
 
     if include_header:
-        print("\nheader:")
-        print(json.dumps(response.headers, indent=4))
-
         return (json.loads(json.dumps(dict(response.headers))), json.loads(response.text))
     else:
         return json.loads(response.text)
 
 def api_url(query_url, include_header=False):
     response = requests.get(query_url, auth=('theseatoms', config.PASSWORD))
+
     if include_header:
-        print("\nheader:")
-        print(response.headers)
-        
-        print(type(response.headers ))
-        ret1 = json.loads(json.dumps(dict(response.headers)) )
-        ret2 = json.loads(response.text)
-        
-        return (ret1, ret2)
+        return (json.loads(json.dumps(dict(response.headers))), json.loads(response.text))
     else:
         return json.loads(response.text)
+
+
+def build_direction_dict(header):
+    directions_dict = {}
+    pages_links = header["link"]
+    pages_data = pages_links.split(',')
+
+    for link in pages_data:
+        value_key = link.split(';')    
+        key = re.search('"([^"]*)"' , value_key[1]).group(0)[1:-1] # word betweeen double quotes
+        value = value_key[0][1:-1]
+         
+        directions_dict[key] = value
+
+    return directions_dict
+
+def download_content(forks_data):
+   pass 
+
+
+
 
 # -------------------------------------------------------------
 
@@ -59,23 +71,45 @@ output_filename = "recipe_paths"
 data = api("/repos/LarryMad/recipes")
 larry_forks_url = data["forks_url"]
 
-
-# TODO loop through paginated results
+# get all forks, paginated links live in the header response
 header, larry_forks_data = api_url(larry_forks_url, include_header=True)
-print(json.dumps(header, indent=4))
-gerd = raw_input()
 
+# parse links and build dict of "first", "next", "last", "prev" dict
+directions_dict = build_direction_dict(header)
+
+page = 1
+remaining_pages = int(directions_dict["last"][directions_dict["last"].index('=')+1:]) - 1
+
+# loop through paginated results
+while remaining_pages > 0:
+    
+    for fork_data in larry_forks_data:
+        download_content(fork_data)
+    
+    print(json.dumps(header, indent=4))
+
+    header, larry_forks_data = api_url(directions_dict["next"], include_header=True)
+    
+    directions_dict = build_direction_dict(header)
+    
+    print("next: ", directions_dict["next"])
+
+    next_page = int(directions_dict["next"][directions_dict["next"].index('=')+1:])
+    last_page = int(directions_dict["last"][directions_dict["last"].index('=')+1:])
+    remaining_pages = last_page - next_page + 1
+    print(next_page, last_page, remaining_pages)
+    gerd = raw_input("pause ")
+    
+     
 print(json.dumps(larry_forks_data, indent=4))
 print(larry_forks_data[0].keys())
 
-gerd = raw_input()
 
-print("num of forks: ", len(larry_forks_data))
-
-gerd = raw_input()
 
 
 for fork_data in larry_forks_data:
+
+
     GET_sha = "/repos/" + fork_data["full_name"] + "/git/refs/"
     data = api(GET_sha)
     
@@ -90,19 +124,14 @@ for fork_data in larry_forks_data:
 
     GET_commit = "/repos/" + fork_data["full_name"] + "/git/commits/" + sha
     data = api(GET_commit)
-    
     tree_sha = data["tree"]["sha"] # why? to ensure latest, "master" branch
 
     GET_tree = "/repos/" + fork_data["full_name"] + "/git/trees/" + tree_sha
     data = api(GET_tree)
 
-
     paths = [] 
 
     for file_data in data["tree"]:
-        #print(file_data) 
-        #print(file_data["url"])
-        
         GET_contents = "/repos/" + fork_data["full_name"] + "/contents/" + file_data["path"]
         contents = api(GET_contents)
                 
@@ -111,7 +140,7 @@ for fork_data in larry_forks_data:
         
         new_file_dir = os.getcwd() + "/knives/" + owner + "/"
         new_file_path = os.getcwd() + "/knives/" + owner + "/" + file_data["path"] 
-        print(new_file_path)
+        print("new file :", new_file_path)
         
         if not os.path.exists(new_file_dir):
             os.makedirs(new_file_dir) 
